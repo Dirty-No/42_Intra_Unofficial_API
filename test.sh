7837e0cabc77d648ca9e454a3022713df3f3af70
Noëlle <54525684+Dirty-No@users.noreply.github.com>
2020-09-03 02:25:19 +0300

    Delete test.sh

--- test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -e
-
-LINK="https://profile.intra.42.fr/searches/search?query=$1"
-curl  $LINK \
-    --cookie cookies.txt \
-    -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9'   \
-    --compressed 2> /dev/null | sed -n '/Users/,$p' | sed '/General/,$d' |  sed 's/<[^<>]*>//g' | sed 's/Users//g' | sed -r '/^\s*$/d'
