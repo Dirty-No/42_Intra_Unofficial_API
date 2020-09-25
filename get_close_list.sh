d8d1ad8b179aa8413ab3264cc2b0283448427c7b
smaccary <smaccary@student.42.fr>
2020-09-25 23:32:55 +0200

    improved close_list (2nd page, date)

--- get_close_list.sh
+++ get_close_list.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -e
+#!/bin/bash
 
 # SCRIPT TO GET ALL LAST COMMUNITY SERVICES (42paris only)
 
@@ -12,9 +12,16 @@
 declare -a StringArray=("assembly" "order" "federation" "alliance")
 
 for coalition in "${StringArray[@]}"; do
-    LINK="https://profile.intra.42.fr/blocs/16/coalitions/42cursus-paris-the-$coalition/scores?search=close&commit=Search"
-    curl $LINK \
-    -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9'   \
-    --cookie cookies.txt \
-    --compressed 2> /dev/null | grep href=\"/users/ | sed 's/<[^<>]*>//g'
+    printf "\n%s %10s %s\n\n" "###############" "$coalition" "###############"
+    for page in {1..2}; do
+        LINK="https://profile.intra.42.fr/blocs/16/coalitions/42cursus-paris-the-$coalition/scores?page=$page&search=close"
+        PAGE=$(
+            curl -s $LINK \
+            -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9'   \
+            --cookie cookies.txt \
+            --compressed)
+        LOGINS=$(echo "$PAGE" | grep href=\"/users/ | sed 's/<[^<>]*>//g')
+        DATES=$(echo "$PAGE" | grep -B6 href=\"/users/ | grep -Eo title=\'.*\' | sed 's/title=//g' | tr "'" '|' )
+        paste <(echo "$DATES") <(echo "$LOGINS")
+    done
 done
